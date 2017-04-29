7b62cc058af6463ebc742d538fb7410482968a39
Ross Miller <ross@heal.com>
2017-04-29 11:21:11 -0700

    style choose case

--- client/src/components/ChooseCase.js
+++ client/src/components/ChooseCase.js
@@ -12,32 +12,19 @@ export default class Home extends React.Component {
     		<h2>Select A Case</h2>
     		<div className='foster-cases'>
     			<div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-    				<h4>LastName <span>NO. 23765772</span></h4>
-    			</div>
-    			<div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-    				<h4>LastName <span>NO. 23765772</span></h4>
-    			</div>
-    			<div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-    				<h4>LastName <span>NO. 23765772</span></h4>
-    			</div>
-    			<div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-    				<h4>LastName <span>NO. 23765772</span></h4>
-    			</div>
-    			<div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-                    <h4>LastName <span>NO. 23765772</span></h4>
+                    <h4><span className="lightgrey">Child:</span> Rhoades, Robert (M, 14)</h4>
+                    <h4><span className="lightgrey">Mother:</span> Smith, Candice</h4>
                 </div>
+
                 <div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-                    <h4>LastName <span>NO. 23765772</span></h4>
+                    <h4><span className="lightgrey">Child:</span> O'Riley, Baba (M, 14)</h4>
+                    <h4><span className="lightgrey">Mother:</span> Peters, Jill</h4>
                 </div>
+
                 <div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-                    <h4>LastName <span>NO. 23765772</span></h4>
+                    <h4><span className="lightgrey">Child:</span> Roberto, José (M, 14)</h4>
+                    <h4><span className="lightgrey">Mother:</span> Roberto, María</h4>
                 </div>
-                <div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-                    <h4>LastName <span>NO. 23765772</span></h4>
-                </div>
-                <div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-    				<h4>LastName <span>NO. 23765772</span></h4>
-    			</div>
     		</div>
     	</div>
     )
