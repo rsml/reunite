dd4cc77f41920f5be055f2614bddd7c07b008bf4
Davith <daviddaravanh@gmail.com>
2017-04-29 02:37:51 -0700

    created success page

--- client/src/components/SuccessPage.js
+++ client/src/components/SuccessPage.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router"
 
 // Home page component
 export default class Home extends React.Component {
   // render
   render() {
     return (
-      <div className="page-home">
-        <h4>Choose Date Page</h4>
+      <div className="page-home success-wrapper">
+        <Link to='/'>
+          <a id="cancel-icon">
+            <i className="glyphicon glyphicon-remove-circle"></i>
+            <p>Close</p>
+          </a> 
+        </Link>  
+        <div className="success-message">
+          <i className="glyphicon glyphicon-ok-circle"></i>
+          <h1>SUCCESS! YAY!</h1>
+        </div>  
       </div>
     );
   }
