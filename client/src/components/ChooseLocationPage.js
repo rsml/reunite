b44a986021e311c5de192d4770d813f4dac22ff0
Ross Miller <ross@heal.com>
2017-04-29 00:38:52 -0700

    Add market to google map

--- client/src/components/ChooseLocationPage.js
+++ client/src/components/ChooseLocationPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PlacesAutocomplete from 'react-places-autocomplete'
+import PlacesAutocomplete, {geocodeByAddress, geocodeByPlaceId} from 'react-places-autocomplete'
 
 // Home page component
 export default class Home extends React.Component {
@@ -12,11 +12,33 @@ export default class Home extends React.Component {
   handleFormSubmit(event) {
     event.preventDefault()
 
+    const that = this;
+    debugger;
     geocodeByAddress(this.state.address,  (err, latLng) => {
+      debugger;
       if (err) { console.log('Oh no!', err) }
 
-      console.log(`Yay! Got latitude and longitude for ${address}`, latLng)
+      console.log(`Yay! Got latitude and longitude for ${that.state.address}`, latLng)
+      that.drawMap(latLng);
     })
+  }
+
+  drawMap(latLng) {
+    if(!window.google){
+      return;
+    }
+
+    var mapProp= {
+        center:latLng,
+        zoom:5,
+    };
+    var map=new window.google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      title: 'Hello World!'
+    });
   }
 
   render() {
@@ -30,9 +52,26 @@ export default class Home extends React.Component {
 
       if(window.google && typeof window.google !== 'undefined') {
         return (
-          <div className="flex">
-            <PlacesAutocomplete className="flex-grow" inputProps={inputProps} />
-            <input type="submit" value="Search" />
+          <div>
+            <div className="flex pull-to-front width-100">
+              <div className="flex-grow">
+                <PlacesAutocomplete inputProps={inputProps} />
+              </div>
+              <input type="submit" value="Search" onClick={that.handleFormSubmit.bind(that)} />
+            </div>
+            <div className="z-index-1">
+
+              <div id="googleMap"></div>
+            </div>
+
+            <div>
+              {that.state.address}
+            </div>
+
+            <div className="cta">
+              CONFIRM
+            </div>
+
           </div>
         )
       }else{
