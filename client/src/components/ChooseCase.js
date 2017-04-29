346953d4bfddac9898c93eba6a63836fceee5aae
Ross Miller <ross@heal.com>
2017-04-29 11:55:38 -0700

    Update ages:

--- client/src/components/ChooseCase.js
+++ client/src/components/ChooseCase.js
@@ -12,17 +12,17 @@ export default class Home extends React.Component {
     		<h2>Select A Case</h2>
     		<div className='foster-cases'>
     			<div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-                    <h4><span className="lightgrey">Child:</span> Rhoades, Robert (M, 14)</h4>
+                    <h4><span className="lightgrey">Child:</span> Rhoades, Miley (F, 14)</h4>
                     <h4><span className="lightgrey">Mother:</span> Smith, Candice</h4>
                 </div>
 
                 <div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-                    <h4><span className="lightgrey">Child:</span> O'Riley, Baba (M, 14)</h4>
+                    <h4><span className="lightgrey">Child:</span> O'Riley, Baba (M, 9)</h4>
                     <h4><span className="lightgrey">Mother:</span> Peters, Jill</h4>
                 </div>
 
                 <div className='individual-case' onClick={(() => browserHistory.push("choose-date"))}>
-                    <h4><span className="lightgrey">Child:</span> Roberto, José (M, 14)</h4>
+                    <h4><span className="lightgrey">Child:</span> Roberto, José (M, 17)</h4>
                     <h4><span className="lightgrey">Mother:</span> Roberto, María</h4>
                 </div>
     		</div>
